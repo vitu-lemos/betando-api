09256a4a57f6a23ce8f1dcadb10812c65589a761
Vitor Lemos <lemos.dev02@gmail.com>
2022-11-15 02:42:49 -0300

    add gitignore

--- dist/enviroment.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv").config({ path: `./.env.${process.env.NODE_ENV}` });
-exports.default = {
-    env: process.env.NODE_ENV,
-    BETANO_BASE_URL: process.env.BETANO_BASE_URL,
-    PORT: process.env.PORT,
-};
